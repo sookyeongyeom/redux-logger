0119f0a6a1f726e6ba52df8ee8f578edfa776224
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-07 06:06:04 +0900

    Feat: redux-thunk를 사용하여 비동기 함수를 디스패치

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,13 +1,13 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter';
+import { increase, decrease, increaseAsync, decreaseAsync } from '../modules/counter';
 
 const CounterContainer = () => {
 	const number = useSelector(({ counter }) => counter);
 	const dispatch = useDispatch();
-	const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
-	const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+	const onIncrease = useCallback(() => dispatch(increaseAsync()), [dispatch]);
+	const onDecrease = useCallback(() => dispatch(decreaseAsync()), [dispatch]);
 
 	return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 };
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -6,7 +6,9 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치하는 함수
+// redux-thunk와 같이 함수를 액션으로 디스패치할 수 있게 도와주는 미들웨어가 없다면
+// 이 함수는 디스패치되지 못함
 export const increaseAsync = () => (dispatch) => {
 	setTimeout(() => {
 		dispatch(increase());
