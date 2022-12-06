9d1b506d926cc60ee6b0baaf3ea672fdcc21a112
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-07 05:57:09 +0900

    Feat: redux-thunk 적용

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import rootReducer from './modules/index';
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
