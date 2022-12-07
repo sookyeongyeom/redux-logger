5bc42c769df7a1f2a1aa9831b3dfc1e4ce587568
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-07 19:17:37 +0900

    Feat: Sample 컨테이너에서 요청 실패 예외 처리

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -17,8 +17,18 @@ const SampleContainer = () => {
 	const getUsersDsp = useCallback(() => dispatch(getUsers()), [dispatch]);
 
 	useEffect(() => {
-		getPostDsp(1);
-		getUsersDsp();
+		// async 함수는 useEffect의 파라미터로 넣을 수 없기 때문에
+		// 그 내부에서 async 함수를 선언하고 호출해주어야 함
+		const fn = async () => {
+			try {
+				await getPostDsp(1);
+				await getUsersDsp();
+			} catch (e) {
+				// 요청 실패 예외 처리
+				console.log(e);
+			}
+		};
+		fn();
 	}, [getPostDsp, getUsersDsp]);
 
 	return <Sample post={post} users={users} loadingPost={loadingPost} loadingUsers={loadingUsers} />;
