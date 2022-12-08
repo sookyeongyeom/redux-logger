81fbb19aa6bca0fc2b8f93610094a606da37b0fc
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-08 19:06:17 +0900

    Feat: 루트 사가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
+import counter, { counterSaga } from './counter';
 import sample from './sample';
 import loading from './loading';
+import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({ counter, sample, loading });
+
+// 루트 사가 생성
+export function* rootSaga() {
+	// all은 여러 사가를 합쳐주는 역할을 함
+	yield all([counterSaga()]);
+}
 
 export default rootReducer;
